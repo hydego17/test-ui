56836bdcbfbb6c3efe6b75a943959c29fbf1b7b2
Umma Ahimsha <uahimsha@gmail.com>
2024-02-13 01:42:07 +0700

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { libInjectCss, scanEntries } from "vite-plugin-lib-inject-css";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   resolve: {
