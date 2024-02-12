8655a6d61dba090be9815a45234d3d2614838543
Umma Ahimsha <uahimsha@gmail.com>
2024-02-13 00:46:25 +0700

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { peerDependencies } from "./package.json";
-import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,16 +12,19 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: "./src/index.ts", // Specifies the entry point for building the library.
-      name: "@hydego17/test-ui", // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "@hydego17/test-ui",
+      fileName: "test-ui",
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
-    sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
   },
-  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [react(), dts({ rollupTypes: true })],
 });
